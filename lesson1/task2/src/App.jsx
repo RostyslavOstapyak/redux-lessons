b78498d1bcc43a1ff53963527b085832aa3399df
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-10-20 18:57:52 +0300

    lesson 1.2 t1

--- lesson1/task2/src/App.jsx
+++ lesson1/task2/src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import Header from './Header.jsx';
+import { userData } from './userData-context';
 
-const App = () => {
-  return <div>Hello, React!</div>;
-};
+class App extends React.Component {
+  state = {
+    userData: userData.teslaUser,
+  };
 
-//sasas
+  // eslint-disable-next-line class-methods-use-this
+  render() {
+    console.log(this.state);
+    return (
+      <div className="page">
+        <Header />
+      </div>
+    );
+  }
+}
 
 export default App;
