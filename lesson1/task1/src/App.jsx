408c8811aae11bb1f7c15a03de2368ec260ef490
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-10-20 18:44:26 +0300

    lesson1.1 t3

--- lesson1/task1/src/App.jsx
+++ lesson1/task1/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import ThemedButton from './ThemedButton.jsx';
-import { themes, ThemeContext } from './themes-context';
+import ThemedButton from './ThemedButton';
+import { themes, ThemeContext } from './themes-context.js';
 
 class App extends React.Component {
   state = {
@@ -10,7 +10,9 @@ class App extends React.Component {
   toggleTheme = () => {
     const newTheme = this.state.theme === themes.dark ? themes.light : themes.dark;
 
-    this.setState({ theme: newTheme });
+    this.setState({
+      theme: newTheme,
+    });
   };
 
   render() {
--- lesson1/task1/src/ThemedButton.jsx
+++ lesson1/task1/src/ThemedButton.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { ThemeContext } from './themes-context';
+import { ThemeContext } from './themes-context.js';
 
-class ThemedButton extends React.PureComponent {
+class ThemedButton extends React.Component {
   render() {
     return (
       <button
         {...this.props}
-        style={{ backgroundColor: this.context.background, color: this.context.fontColor }}
         className="btn"
+        style={{
+          backgroundColor: this.context.background,
+          color: this.context.fontColor,
+        }}
       />
     );
   }
