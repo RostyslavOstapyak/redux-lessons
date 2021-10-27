58f1afb3fc1849f89fac81d9d1b7d5cc3d49792d
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-10-28 02:21:37 +0300

    lesson 3.3 t9

--- lesson3/task3/src/index.js
+++ lesson3/task3/src/index.js
@@ -1,7 +1,3 @@
-import { addItem, removeItem } from './cart.actions';
 import store from './store';
 
 store.subscribe(() => console.log(store.getState()));
-
-store.dispatch(addItem({ id: 1, title: 'fff' }));
-store.dispatch(removeItem(1));
