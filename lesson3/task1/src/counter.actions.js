24561af58fa1b51193b23e30c8d0e518bff963d9
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-10-28 00:52:34 +0300

    lesson 3.1 t1

---
+++ lesson3/task1/src/counter.actions.js
@@ -0,0 +1,7 @@
+export const INCREMENT = 'COUNTER/INCREMENT';
+export const DECREMENT = 'COUNTER/DECREMENT';
+export const RESET = 'COUNTER/RESET';
+
+export const increment = () => ({ type: INCREMENT });
+export const decrement = () => ({ type: INCREMENT });
+export const reset = () => ({ type: INCREMENT });
