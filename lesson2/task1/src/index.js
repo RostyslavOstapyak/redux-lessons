98d572696af97a352aeb51f5a43828a250cef3fa
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-10-27 21:34:03 +0300

    lesson2.1t1

---
+++ lesson2/task1/src/index.js
@@ -0,0 +1,31 @@
+import { createStore } from 'redux';
+
+const INCREMENT = 'COUNTER/INCREMENT';
+const DECREMENT = 'COUNTER/DECREMENT';
+
+const incrementActionCreator = () => ({
+  type: INCREMENT,
+});
+const decrementActionCreator = () => ({
+  type: DECREMENT,
+});
+
+const counterReducer = (state = 0, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return state + 1;
+    case DECREMENT:
+      return state - 1;
+    default:
+      return state;
+  }
+};
+
+const store = createStore(counterReducer);
+
+store.dispatch(incrementActionCreator());
+store.dispatch(incrementActionCreator());
+store.dispatch(incrementActionCreator());
+store.dispatch(decrementActionCreator());
+
+console.log(store.getState());
