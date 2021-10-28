eb6ec4cc652bc69551434464821b6fe40fdd31c7
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-10-28 05:11:05 +0300

    lesson 4.3 t1

--- lesson4/task3/src/users/Pagination.jsx
+++ lesson4/task3/src/users/Pagination.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 const Pagination = ({ goNext, goPrev, currentPage, itemsPerPage, totalItems }) => {
   const isNextDisabled = totalItems / itemsPerPage > currentPage + 1;
   const isPrevDisabled = currentPage <= 0;
-  console.log(isNextDisabled);
   return (
     <div className="pagination">
       <button className="btn" onClick={goPrev} disabled={isPrevDisabled}>
