4861f3fe41402f83707d3cc0c095e295ab4be5c7
Rostyslav Ostapyak <rostyslav.ostapyak@gmail.com>
2021-11-02 04:19:35 +0200

    removed logic from component to task actions aded ability to update/remove/create task

--- ToDoList/src/tasks/components/TasksList.jsx
+++ ToDoList/src/tasks/components/TasksList.jsx
@@ -3,50 +3,25 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Task from './Task.jsx';
 import CreateTaskInput from './CreateTaskInput.jsx';
-import { createTask, fetchTasksList, updateTask, deleteTask } from '../tasksGateway.jsx';
 import * as tasksActions from '../tasks.actions';
-import { tasksListSelector } from '../tasks.selectors';
+import { sortedTasksListSelector } from '../tasks.selectors';
 
 class TasksList extends React.Component {
   componentDidMount() {
     this.props.getTaskList();
   }
 
-  onCreate = text => {
-    const newTask = {
-      text,
-      done: false,
-    };
-
-    return createTask(newTask).then(() => this.fetchTasks());
-  };
-
-  handleTaskStatusChange = id => {
-    const { done, text } = this.state.tasks.find(task => task.id === id);
-    const updatedTask = {
-      text,
-      done: !done,
-    };
-    updateTask(id, updatedTask).then(() => this.fetchTasks());
-  };
-
-  handleTaskDelete = id => {
-    deleteTask(id).then(() => this.fetchTasks());
-  };
-
   render() {
-    const sortedList = this.props.tasks.slice().sort((a, b) => a.done - b.done);
-
     return (
       <main className="todo-list">
-        <CreateTaskInput onCreate={this.onCreate} />
+        <CreateTaskInput onCreate={this.props.createTask} />
         <ul className="list">
-          {sortedList.map(task => (
+          {this.props.tasks.map(task => (
             <Task
               key={task.id}
               {...task}
-              onChange={this.handleTaskStatusChange}
-              onDelete={this.handleTaskDelete}
+              onChange={this.props.updateTask}
+              onDelete={this.props.deleteTask}
             />
           ))}
         </ul>
@@ -57,15 +32,21 @@ class TasksList extends React.Component {
 
 TasksList.propTypes = {
   getTaskList: PropTypes.func.isRequired,
+  updateTask: PropTypes.func.isRequired,
+  createTask: PropTypes.func.isRequired,
+  deleteTask: PropTypes.func.isRequired,
   tasks: PropTypes.arrayOf(PropTypes.shape()),
 };
 
 const mapDispatch = {
   getTaskList: tasksActions.getTaskList,
+  updateTask: tasksActions.updateTask,
+  deleteTask: tasksActions.deleteTask,
+  createTask: tasksActions.createTask,
 };
 
 const mapState = state => ({
-  tasks: tasksListSelector(state),
+  tasks: sortedTasksListSelector(state),
 });
 
 export default connect(mapState, mapDispatch)(TasksList);
--- ToDoList/src/tasks/tasks.actions.js
+++ ToDoList/src/tasks/tasks.actions.js
@@ -1,4 +1,5 @@
-import { fetchTasksList } from './tasksGateway.jsx';
+import { tasksListSelector } from './tasks.selectors.js';
+import * as taskGateway from './tasksGateway.jsx';
 
 export const TASKS_LIST_RECEIVED = 'TASKS_LIST_RECEIVED';
 
@@ -9,7 +10,41 @@ export const tasksListReceived = tasksList => ({
 
 export const getTaskList = () => {
   const thunkAction = dispatch => {
-    fetchTasksList().then(tasksList => dispatch(tasksListReceived(tasksList)));
+    taskGateway.fetchTasksList().then(tasksList => dispatch(tasksListReceived(tasksList)));
+  };
+  return thunkAction;
+};
+
+export const updateTask = taskId => {
+  const thunkAction = (dispatch, getState) => {
+    const state = getState();
+    const tasksList = tasksListSelector(state);
+
+    const task = tasksList.find(task => task.id === taskId);
+    const updatedTask = {
+      ...task,
+      done: !task.done,
+    };
+
+    taskGateway.updateTask(taskId, updatedTask).then(() => dispatch(getTaskList()));
+  };
+  return thunkAction;
+};
+
+export const deleteTask = taskId => {
+  const thunkAction = dispatch => {
+    taskGateway.deleteTask(taskId).then(() => dispatch(getTaskList()));
+  };
+  return thunkAction;
+};
+
+export const createTask = text => {
+  const thunkAction = dispatch => {
+    const taskData = {
+      text,
+      done: false,
+    };
+    taskGateway.createTask(taskData).then(() => dispatch(getTaskList()));
   };
   return thunkAction;
 };
